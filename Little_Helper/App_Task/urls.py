68e3e654ef051bda3ee31fa8fc38d8955347e958
DannyFabs <aderemifawoyed@gmail.com>
2023-08-13 13:29:07 -0500

    Implemented functions in views.py to handle deleting and editing tasks(decided to change slug fields only if the task name is changed)

--- Little_Helper/App_Task/urls.py
+++ Little_Helper/App_Task/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('create_task/', views.create_task, name='create_task'),
     path('task/<slug:slug>/', views.view_task, name='current_task'),
+    path('delete/<slug:slug>/', views.delete_task, name='delete'),
+    path('edit_task/<slug:slug>', views.edit_task, name='edit_task'),
+
 ]
--- Little_Helper/App_Task/views.py
+++ Little_Helper/App_Task/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -9,10 +10,12 @@ import uuid
 from .models import Task
 
 
+@login_required
 def home(request):
     return render(request, 'App_Task/home.html', context={'title': 'Home'})
 
 
+@login_required
 def create_task(request):
     form = CreateTask()
     if request.method == 'POST':
@@ -28,7 +31,38 @@ def create_task(request):
     return render(request, 'App_Task/create_tsk.html', context={'title': 'Create Task', 'form': form})
 
 
+@login_required
 def view_task(request, slug):
     curr_task = Task.objects.get(task_slug=slug)
 
     return render(request, 'App_Task/task.html', context={'task': curr_task, 'title': 'Task'})
+
+
+@login_required
+def delete_task(request, slug):
+    task = Task.objects.get(task_slug=slug)
+    task.delete()
+
+    return render(request, 'App_Task/home.html', context={})
+
+
+@login_required
+def edit_task(request, slug):
+    # using reverse relationship between user and task
+    current_task = request.user.all_tasks.get(task_slug=slug)
+    form = CreateTask(instance=current_task)
+
+    init_name = current_task.task_name
+
+    if request.method == "POST":
+        form = CreateTask(request.POST, instance=current_task)
+        if form.is_valid:
+            task = form.save(commit=False)
+            #         update slug, incase the task name is changed
+            name = task.task_name
+            if init_name != name:
+                task.task_slug = name.replace(" ", "-") + "-" + str(uuid.uuid4())
+            task.save()
+            return HttpResponseRedirect(reverse('App_Task:current_task', kwargs={'slug': task.task_slug}))
+
+    return render(request, 'App_Task/edit_task.html', context={'task': current_task, 'form': form})
